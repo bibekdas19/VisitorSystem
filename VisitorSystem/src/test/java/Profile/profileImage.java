6d6cf903b691c5eb9cb1a0a14e916a7f84d7bcf7
unknown <learner.bibekdas@gmail.com>
2025-04-23 17:04:00 +0545

    profile all complete.

--- VisitorSystem/src/test/java/Profile/profileImage.java
+++ VisitorSystem/src/test/java/Profile/profileImage.java
@@ -56,7 +56,7 @@ public class profileImage {
 				.contentType("application/json")
 				.body(jsonBody)
 				.when()
-				.post("authenticate")
+				.post("/authenticate")
 				.then()
 				.statusCode(200)
 				.log().all()
@@ -64,3 +64,334 @@ public class profileImage {
 		AuthToken = Authresponse.getHeader("X-AUTH-TOKEN");
 
 }
+	@Test
+	public void getProfileImage() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(200)
+	            .extract().response();
+		//check if the token is in the present in the response
+        assertTrue(response.getHeaders().hasHeaderWithName("X-AUTH-TOKEN"), "Missing X-AUTH-TOKEN header");
+        System.out.println("Request ID: " + response.getHeaders().hasHeaderWithName("X-AUTH-TOKEN"));
+	}
+	
+	@Test
+	public void getDocumentImage() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "document")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(200)
+	            .extract().response();
+		//check if the token is in the present in the response
+        assertTrue(response.getHeaders().hasHeaderWithName("X-AUTH-TOKEN"), "Missing X-AUTH-TOKEN header");
+        System.out.println("Request ID: " + response.getHeaders().hasHeaderWithName("X-AUTH-TOKEN"));
+		
+	}
+	
+	@Test
+	public void getProfileImagewithoutDevice() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(400)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+        String code = response.jsonPath().getString("code");
+        String description = response.jsonPath().getString("description");
+
+        assertNotNull(description, "Description is missing from the response");
+        assertNotNull(code, "Code is missing");
+        assertFalse(description.isEmpty(), "Description is empty");
+        assertFalse(code.isEmpty(), "Code is empty");
+        assertEquals(code,"GNR_PARAM_MISSING");
+        assertEquals(description,"Bad Request.");
+		
+	}
+    @Test
+	public void getProfileImagewithoutGeo() {
+    	Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(400)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+        String code = response.jsonPath().getString("code");
+        String description = response.jsonPath().getString("description");
+
+        assertNotNull(description, "Description is missing from the response");
+        assertNotNull(code, "Code is missing");
+        assertFalse(description.isEmpty(), "Description is empty");
+        assertFalse(code.isEmpty(), "Code is empty");
+        assertEquals(code,"GNR_PARAM_MISSING");
+        assertEquals(description,"Bad Request.");
+		
+	}
+	@Test
+	public void getProfileImagewithoutUserAgent() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(400)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+        String code = response.jsonPath().getString("code");
+        String description = response.jsonPath().getString("description");
+
+        assertNotNull(description, "Description is missing from the response");
+        assertNotNull(code, "Code is missing");
+        assertFalse(description.isEmpty(), "Description is empty");
+        assertFalse(code.isEmpty(), "Code is empty");
+        assertEquals(code,"GNR_PARAM_MISSING");
+        assertEquals(description,"Bad Request.");
+		
+	}
+	@Test
+	public void getProfileImagewithoutAuth() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(400)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+        String code = response.jsonPath().getString("code");
+        String description = response.jsonPath().getString("description");
+
+        assertNotNull(description, "Description is missing from the response");
+        assertNotNull(code, "Code is missing");
+        assertFalse(description.isEmpty(), "Description is empty");
+        assertFalse(code.isEmpty(), "Code is empty");
+        assertEquals(code,"GNR_PARAM_MISSING");
+        assertEquals(description,"Bad Request.");
+		
+	}
+	@Test
+	public void getProfileImagewithInvalidDevice() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(422)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_INVALID_DATA");
+		 assertEquals(description,"Invalid data.");
+	}
+	
+	@Test
+	public void getProfileImagewithInvalidLocation() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(422)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_INVALID_DATA");
+		 assertEquals(description,"Invalid data.");
+		
+	}
+	@Test
+	public void getProfileImagewithInvalidAuth() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(401)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_AUTHENTICATION_FAIL");
+		 assertEquals(description,"Authentication Failed.");
+		
+	}
+	@Test
+	public void getProfileImagewithServerDown() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "portrait")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(401)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_ERR");
+		 assertEquals(description,"Internal Server Error");
+		
+		
+	}
+	
+	@Test
+	public void getProfileImagewithoutparam() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(400)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_PARAM_MISSING");
+	     assertEquals(description,"Bad Request.");
+		
+	}
+	@Test
+	public void getProfileImagewithoutImagepushed() {
+		Response response = given()
+	            .header("X-GEO-Location", "12,12")
+	            .header("X-Device-Id", "")
+	            .header("X-AUTH-TOKEN",AuthToken)
+	            .header("User-Agent", "NepalTravelApp/1.0.0 android")
+	            .pathParam("type", "")
+	        .when()
+	            .get("/profile")
+	        .then()
+	            .statusCode(404)
+	            .extract().response();
+		
+		// Extracting and asserting response values
+		String code = response.jsonPath().getString("code");
+	     String description = response.jsonPath().getString("description");
+	     String signature = response.jsonPath().getString("signature");
+	     
+	     assertNotNull(description, "Description is missing from the response");
+	     assertNotNull(code, "Code is missing");
+	     assertNotNull(signature,"signature is missing");
+	     
+	     assertFalse(description.isEmpty(), "Description is empty");
+	     assertFalse(code.isEmpty(), "Code is empty");
+	     assertFalse(signature.isEmpty(),"signature is empty");
+	     
+	     assertEquals(code,"GNR_NOT_FOUND");
+	     assertEquals(description,"Visitor Image not found.");
+		
+	}
+		
+	}
